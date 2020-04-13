d6e53d06a04f3ef46b9545ca74d6dd08b1ce5066
Martin Höher <martin@rpdev.net>
2020-04-13 15:27:52 +0200

    Fix copyright header

--- qlmdb/errors.h
+++ qlmdb/errors.h
@@ -12,7 +12,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with QLMDB.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef ERRORS_H
 #define ERRORS_H
--- qlmdb/transactionprivate.h
+++ qlmdb/transactionprivate.h
@@ -12,7 +12,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with QLMDB.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef TRANSACTIONPRIVATE_H
 #define TRANSACTIONPRIVATE_H
